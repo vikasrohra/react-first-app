0300cf01f84ba663c66a9211d8360f38636493bf
vikasrohra <vikasrohra2@gmail.com>
2023-07-29 02:53:11 +0530

    Added a complex React element tree via React.createElement()

---
+++ app.js
@@ -0,0 +1,6 @@
+const heading1 = React.createElement('h1', {id: 'title1'}, 'Namaste Everyone from React!');
+const heading2 = React.createElement('h2', {id: 'title2'}, 'Namaste Everyone from React!');
+const container = React.createElement('div', {id: 'container'}, [heading1, heading2]);
+console.log(heading1);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
