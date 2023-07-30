7f0567269c22a95970faf5b6a941ce55ede5cb60
vikasrohra <vikasrohra2@gmail.com>
2023-07-30 13:59:50 +0530

    Added parcel to ignite the app, removed React CDN links and added React packages via npm

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement('h1', {id: 'title1'}, 'Namaste Everyone from React!');
 const heading2 = React.createElement('h2', {id: 'title2'}, 'Namaste Everyone from React!');
 const container = React.createElement('div', {id: 'container'}, [heading1, heading2]);
