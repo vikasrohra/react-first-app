33d34b617ae197fbf6a95f0b5bdfd228f228ee1f
vikasrohra <vikasrohra2@gmail.com>
2023-07-31 14:45:05 +0530

    Added start and build scripts in package.json, installed babel-console package to remove the logs, added a browsersList in package.json and added functional component

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement('h1', {id: 'title1'}, 'Namaste Everyone from React!');
-const heading2 = React.createElement('h2', {id: 'title2'}, 'Namaste Everyone from React!');
-const container = React.createElement('div', {id: 'container'}, [heading1, heading2]);
-console.log(heading1);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container);
+// React Element - single line
+const title1 = <h1>This is a single line React element</h1>;
+// React Element - multi line
+const title2 = <h1>This is a multi line React element</h1>;
+// React functional component
+const Title3 = () => {
+    return (
+        <h1>This is a React Functional Component</h1>
+    );
+}
+// React functional component
+const Title4 = () => (
+    <h1>This is another React Functional Component</h1>
+);
+// React functional component
+const Title5 = () => {
+    return <h1>Yet another React Functional Component</h1>
+};
+// React functional component
+const Container = () => {
+  return (
+    <>
+      <h1 id="container">Namaste React</h1>
+      {title1}
+      {title2}
+      <Title3 />
+      {Title4()}
+      <Title5 />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Container />);
