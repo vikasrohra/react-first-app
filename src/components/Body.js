d73946626c88a43d54c07841c20bb378fd7dd0e0
vikasrohra <vikasrohra2@gmail.com>
2023-08-06 04:21:01 +0530

    Added a search functionality and added state variables using useState hook to filter out the restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { RESTAURANT_LIST } from "../constants";
 import RestaurantCard from './RestaurantCard';
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(RESTAURANT_LIST);
+  const [searchText, setSearchText] = useState("");
+
+  const filterData = (searchText, restaurants) => {
+    return restaurants.filter(restaurant => restaurant.data.name.includes(searchText));
+  } 
+
   return (
-    <div className="restaurant-list">
-      {RESTAURANT_LIST.map((restaurant) => {
-        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input tyle="text" className="search-input" placeholder="Search" value={searchText} onChange={e => setSearchText(e.target.value)} />
+        <button className="search-btn" onClick={() => {
+          const data = filterData(searchText, restaurants);
+          setRestaurants(data);
+        }}>Search</button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
+        })}
+      </div>
+    </>
   );
 };
 
