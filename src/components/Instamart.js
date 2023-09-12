87d488d5dcb5475170f29c1b0d0a52c8498baf8f
vikasrohra <vikasrohra2@gmail.com>
2023-09-12 21:00:30 +0530

    Added an accordion on instamart page

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+    return (
+        <div className="border border-black p-2 m-2">
+            <h3 className="font-bold text-xl">{title}</h3>
+            {!isVisible && <button className="underline" onClick={() => setIsVisible(true)}>Show</button>}
+            {isVisible && <button className="underline" onClick={() => setIsVisible(false)}>Hide</button>}
+            {isVisible && <p>{description}</p>}
+        </div>
+    );
+}
+
 const Instamart = () => {
+    const [visibleSection, setVisibleSection] = useState("about");
     return (
         <div>
-            <h1>Instamart</h1>
+            <h1 className="text-3xl font-bold p-2 m-2">Instamart</h1>
+            <Section isVisible={visibleSection === "about"} setIsVisible={(flag) =>
+                flag ? setVisibleSection("about") : setVisibleSection("")
+            } title={"About Instamart"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"} />
+            <Section isVisible={visibleSection === "team"} setIsVisible={(flag) =>
+                flag ? setVisibleSection("team") : setVisibleSection("")} title={"Team Instamart"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"} />
+            <Section isVisible={visibleSection === "careers"} setIsVisible={(flag) =>
+                flag ? setVisibleSection("careers") : setVisibleSection("")} title={"Careers"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"} />
         </div>
     );
 }
